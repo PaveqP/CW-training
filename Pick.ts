65212adcb312b86f10e6aed9859fbc4a84ca1dfb
Pavel Cherepov <p.cherepov@yadro.com>
2024-07-07 13:26:54 +0300

    mapped types (custom Pick)

---
+++ Pick.ts
@@ -0,0 +1,14 @@
+type customPick<T, K extends keyof T> = {
+    [key in K]: T[key]
+}
+
+interface ITodo {
+    title: string
+    description: string
+    completed: boolean
+}
+
+const new_obj: customPick<ITodo, 'description' | 'completed'> = {
+    description: 'vd',
+    completed: false
+}
